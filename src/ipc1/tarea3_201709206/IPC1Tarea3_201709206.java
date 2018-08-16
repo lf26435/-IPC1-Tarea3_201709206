85717b87c18972e57395afb1d12d54b38abb8756
Luisa Fernanda CC <Luisa Fernanda CC@Laptop-LuisaFernandaCC>
2018-08-16 14:08:46 -0600

    Tarea 3 inicializada

--- src/ipc1/tarea3_201709206/IPC1Tarea3_201709206.java
+++ src/ipc1/tarea3_201709206/IPC1Tarea3_201709206.java
@@ -5,6 +5,8 @@
  */
 package ipc1.tarea3_201709206;
 
+import java.util.Scanner;        
+
 /**
  *
  * @author Luisa Fernanda CC
@@ -16,6 +18,18 @@ public class IPC1Tarea3_201709206 {
      */
     public static void main(String[] args) {
         // Inicializando tarea
+        // Menú Principal
+        System.out.println("1. Usuarios");
+            System.out.println("2. Contador de Dígitos Repetidos");
+            System.out.println("3. Contador de Menor a Mayor");
+            System.out.println("4. Calcular Pomedio");
+            System.out.println("5. Salir");
+        // Declarando variables enteras
+        int op;//op:opción elegida
+        Scanner a = new Scanner(System.in);
+        op = a.nextInt();
+        //
+        
     }
     
 }
