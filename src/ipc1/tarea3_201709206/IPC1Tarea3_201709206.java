e02460923ee8aad2cab018639c5b71227142c004
Luisa Fernanda CC <Luisa Fernanda CC@Laptop-LuisaFernandaCC>
2018-08-16 22:08:34 -0600

    Estructura base YA SALIO :D

--- src/ipc1/tarea3_201709206/IPC1Tarea3_201709206.java
+++ src/ipc1/tarea3_201709206/IPC1Tarea3_201709206.java
@@ -7,8 +7,8 @@ package ipc1.tarea3_201709206;
 public class IPC1Tarea3_201709206 {
 
     public static void main(String[] args) {
-        
-        
+        MenuPrincipal IPC1Tarea3_201709206 = new MenuPrincipal();
+        IPC1Tarea3_201709206.MenuP();     
     }
 }
 
