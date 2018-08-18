80be8fe22b690fb743bde308ee6acf490c79dd5b
Luisa Fernanda CC <Luisa Fernanda CC@Laptop-LuisaFernandaCC>
2018-08-18 00:31:09 -0600

    Números de Mayor A Menos terminado Try aún no funciona .-.

--- src/ipc1/tarea3_201709206/MenuPrincipal.java
+++ src/ipc1/tarea3_201709206/MenuPrincipal.java
@@ -1,6 +1,7 @@
 package ipc1.tarea3_201709206;
 // @author Luisa Fernanda CC
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MenuPrincipal {
@@ -9,11 +10,12 @@ public class MenuPrincipal {
         MenuP();
     }
     Scanner leer = new Scanner(System.in);
+    int Num1,Num2,Num3;//Variables a utilizar
     public void MenuP() {//Menú Principal
         // Declarando variable
-        String op;//op:opción elegida
-        op = "0";
-        while (op!="5"){
+        int op;//op:opción elegida
+        op = 0;
+        while (op!=5){
         // Menú Principal
             System.out.println("[IPC1]Tarea3_201709206\n");
             System.out.println("1. Usuarios");
@@ -21,25 +23,25 @@ public class MenuPrincipal {
             System.out.println("3. Tres números de Mayor a Menor");
             System.out.println("4. Calcular Pomedio");
             System.out.println("5. Salir");
-            op = leer.nextLine();
+            op = leer.nextInt();
             switch(op){
-                case "1":
+                case 1:
                     System.out.println("\n\n\n");
                    Usuario();
                 break;
-                case "2":
+                case 2:
                     System.out.println("\n\n\n");
                    Contador();
                 break;
-                case "3":
+                case 3:
                     System.out.println("\n\n\n");
                     MayorAMenor();
                 break;
-                case "4":
+                case 4:
                     System.out.println("\n\n\n");
                     Promedio();
                 break;
-                case "5":
+                case 5:
                     System.exit(0);//Salir del programa
                 break;
                 default://Si no es ninguna de las opciones
@@ -54,17 +56,17 @@ public class MenuPrincipal {
         System.out.println("2. Mostrar Usuarios Ascendentes ");
         System.out.println("3. Mostrar Usuarios Descendentes");
         System.out.println("4. Menú Principal");
-        String op1;//Opción del menú de usuario
-        op1 = leer.nextLine();
+        int op1;//Opción del menú de usuario
+        op1 = leer.nextInt();
         switch (op1){
-            case "1":
+            case 1:
                 
                 break;
-            case "2":
+            case 2:
                 break;
-            case "3":
+            case 3:
                 break;
-            case "4":
+            case 4:
                 System.out.println("\n\n\n");
                 MenuP();
                 break;
@@ -80,16 +82,16 @@ public class MenuPrincipal {
         System.out.println("2. Ingresar número a buscar");
         System.out.println("3. Mostrar número de dígitos");
         System.out.println("4. Menú Principal");
-        String op2;//Opción del menú contador
-        op2 = leer.nextLine();
+        int op2;//Opción del menú contador
+        op2 = leer.nextInt();
         switch (op2){
-            case "1":
+            case 1:
                 break;
-            case "2":
+            case 2:
                 break;
-            case "3":
+            case 3:
                 break;
-            case "4":
+            case 4:
                 System.out.println("\n\n\n");
                 MenuP();
                 break;
@@ -102,17 +104,22 @@ public class MenuPrincipal {
     public void MayorAMenor(){
         System.out.println("TRES NÚMERO DE MAYOR A MENOR\n");
         System.out.println("1. Ingresar Números");
-        System.out.println("2. Mostrar Ordenados");
-        System.out.println("3. Menú Principal");
-        String op3;//Opción del menú MayorAMenor
-        op3 = leer.nextLine();
+        System.out.println("2. Mostrar Ordenados De Mayor A Menor");
+        System.out.println("3. Mostrar Ordenados De Menor A Mayor");
+        System.out.println("4. Menú Principal");
+        int op3;//Opción del menú MayorAMenor
+        op3 = leer.nextInt();
         switch (op3){
-            case "1":
+            case 1:
                 IngresoNum();
                 break;
-            case "2":
+            case 2:
+                OrdenadoMayorMenor();
                 break;
-            case "3":
+            case 3:
+                OrdenadoMenorMayor();
+                break;
+            case 4:
                 System.out.println("\n\n\n");
                 MenuP();
                 break;
@@ -123,22 +130,82 @@ public class MenuPrincipal {
         }
     }
     public void IngresoNum(){
-        int num1,num2,num3;
-        int[] Número;
-        Número = new int[2];
-        
+        int i,Numero;
+        Numero = 0;//Número que el usuario ingresará
+        i = 1;//Contador
+        int Número[] = new int[3];
+        do{
+        System.out.println("Ingrese Número #"+i);
+        try{
+        Numero= leer.nextInt();
+        }catch(InputMismatchException exce){
+            System.out.println("Numeros, no letras");
+        }
+        Número[i-1]= Numero;//Número es la matriz, [i-1] es la fila en que se ingresará el número, "Numero" la variable en que se guardará
+            System.out.println("Número "+Numero+" resgistrado.");//Confirma el registro del número
+            i++;
+            Num1 = Número[0];//Variable donde se guarda el primer número
+            Num2 = Número[1];//Variable donde se guarda el segundo número
+            Num3 = Número[2];//Variable donde se guarda el tercer número
+        }while(i!=4);
+        System.out.println("\n\n\n\nNúmeros ingresados exitosamente\n");
+        MayorAMenor();
+    }
+    public void OrdenadoMayorMenor(){
+        if (Num1>=Num2 && Num2>=Num3) {
+            System.out.println("\n\n\nSu orden es\n"+Num1+" - "+Num2+" - "+Num3);
+        }
+        else  if (Num1>=Num3 && Num3>=Num2) {
+            System.out.println("\n\n\nSu orden es\n"+Num1+" - "+Num3+" - "+Num2);
+        }
+        else  if (Num2>=Num1 && Num1>=Num3) {
+            System.out.println("\n\n\nSu orden es\n"+Num2+" - "+Num1+" - "+Num3);
+        }
+        else   if (Num2>=Num3 && Num3>=Num1) {
+            System.out.println("\n\n\nSu orden es\n"+Num2+" - "+Num3+" - "+Num1);
+        }
+        else   if (Num3>=Num1 && Num1>=Num2) {
+            System.out.println("\n\n\nSu orden es\n"+Num3+" - "+Num1+" - "+Num2);
+        }
+        else   if (Num3>=Num2 && Num2>=Num1) {
+            System.out.println("\n\n\nSu orden es\n"+Num3+" - "+Num2+" - "+Num1);
+        }
+        System.out.println("de Mayor a Menor\n");
+        MayorAMenor();
+    }
+    public void OrdenadoMenorMayor(){
+        if (Num1<=Num2 && Num2<=Num3) {
+            System.out.println("\n\n\nSu orden es\n"+Num1+" - "+Num2+" - "+Num3);
+        }
+        else  if (Num1<=Num3 && Num3<=Num2) {
+            System.out.println("\n\n\nSu orden es\n"+Num1+" - "+Num3+" - "+Num2);
+        }
+        else  if (Num2<=Num1 && Num1<=Num3) {
+            System.out.println("\n\n\nSu orden es\n"+Num2+" - "+Num1+" - "+Num3);
+        }
+        else   if (Num2<=Num3 && Num3<=Num1) {
+            System.out.println("\n\n\nSu orden es\n"+Num2+" - "+Num3+" - "+Num1);
+        }
+        else   if (Num3<=Num1 && Num1<=Num2) {
+            System.out.println("\n\n\nSu orden es\n"+Num3+" - "+Num1+" - "+Num2);
+        }
+        else   if (Num3<=Num2 && Num2<=Num1) {
+            System.out.println("\n\n\nSu orden es\n"+Num3+" - "+Num2+" - "+Num1);
+        }
+        System.out.println("De Menor a Mayor\n");
+        MayorAMenor();
     }
     public void Promedio(){
         System.out.println("CALCULAR PROMEDIO\n");
         System.out.println("1. Mostrar Promedio");
         System.out.println("2. Menú Principal");
         System.out.println("");
-        String op4;//Opción el menú Promedio
-        op4 = leer.nextLine();
+        int op4;//Opción el menú Promedio
+        op4 = leer.nextInt();
         switch (op4){
-            case "1":
+            case 1:
                 break;
-            case "2":
+            case 2:
                 System.out.println("\n\n\n");
                 MenuP();
                 break;
